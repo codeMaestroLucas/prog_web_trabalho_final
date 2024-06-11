42483741271cd5a4c27b44c01d0729f64cdc0b3a
Lucas Samuel <143713992+codeMaestroLucas@users.noreply.github.com>
2024-06-11 13:05:14 -0300

    Backend corrigido parte da tabela relacional Order

---
+++ back/models/mod_products.py
@@ -0,0 +1,14 @@
+from database import Base
+from sqlalchemy import Column, String, Integer, Float
+
+class Product(Base):
+    """Classe de Produtos
+    
+    Estabelece uma estrutura que será usada no Banco de Dados.
+    """
+    __tablename__ = 'products'
+    
+    id = Column(Integer, primary_key= True, autoincrement= True)
+    name = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer)
