8dcd1451bd10e288dd5d7638f0f8a8907af6bbea
codeMaestroLucas <lucas.samuel.ads@gmail.com>
2024-06-12 13:06:18 -0300

    Versão antes do SQLModel

---
+++ prog_web_trabalho_final-dbb0c196322d610a59889ad0f62429138b50a09d/README.md
@@ -0,0 +1,5 @@
+# Problemas
+
+# Ideias
+1. Colocar valores de usuário, produtos e pedidos válidos em um JSON para rodar
+de uma vez e trabalhar com as requisições.
---
+++ prog_web_trabalho_final-dbb0c196322d610a59889ad0f62429138b50a09d/back/models/mod_users.py
@@ -0,0 +1,15 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    """Classe de Usuário
+    
+    Estabelece uma estrutura que será usada no Banco de Dados.
+    """
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key= True, autoincrement= True)
+    name = Column(String)
+    email = Column(String, unique= True)
+    password = Column(String)
+    
